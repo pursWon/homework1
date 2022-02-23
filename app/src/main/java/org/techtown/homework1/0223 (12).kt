b28c1eed1c365cff29d6d54eeb9f7a5fa06098de
홍원기pc <dt95@naver.com>
2022-02-23 15:26:52 +0900

    2022.02.23 집합 사용 예시

---
+++ app/src/main/java/org/techtown/homework1/0223 (12).kt
@@ -0,0 +1,11 @@
+package org.techtown.homework1
+
+fun main() {
+    var map = mapOf<String, String>(Pair("one", "hello"), "two" to "world")
+   println(
+    """
+   map size : ${map.size}
+   map data : ${map.get("one")}, ${map.get("two")}     
+   """
+   )
+}
