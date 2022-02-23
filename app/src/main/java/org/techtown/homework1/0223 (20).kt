f4a059ddea46c1dbaf63cee49115f758a4e2357d
홍원기pc <dt95@naver.com>
2022-02-23 16:31:41 +0900

    2022.02.23 클래스의 멤버

---
+++ app/src/main/java/org/techtown/homework1/0223 (20).kt
@@ -0,0 +1,13 @@
+package org.techtown.homework1
+
+class Zozo {
+    var name = "hong"
+    constructor(name: String) {
+    this.name = name
+}
+fun someFun() {
+    println("name : $name")
+}
+    class SomeClass {}
+
+}
