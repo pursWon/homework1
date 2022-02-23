d384ea5b3c16e23795d893ff45db7e438de8081f
홍원기pc <dt95@naver.com>
2022-02-23 16:04:11 +0900

    2022.02.23 when문을 표현식으로 사용

---
+++ app/src/main/java/org/techtown/homework1/0223 (17).kt
@@ -0,0 +1,12 @@
+package org.techtown.homework1
+
+fun main() {
+    var data = 10
+    val result = when {
+        data <= 0 -> "data is <=0"
+        data > 100 -> "data is > 100"
+        else -> "data is valid"
+
+    }
+    println(result)
+}
