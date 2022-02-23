b2f6d71359edba63d7bd2e5387db442e43deb5e6
홍원기pc <dt95@naver.com>
2022-02-23 15:37:57 +0900

    2022.02.23 if else 문을 표현식으로 사용

---
+++ app/src/main/res/layout/0223 (14).kt
@@ -0,0 +1,14 @@
+package layout
+
+fun data () {
+    var data = 10
+    val result = if (data > 0) {
+        println("data > 0")
+        true
+    } else {
+        println("data <= 0")
+        false
+    }
+    println(result)
+
+}
