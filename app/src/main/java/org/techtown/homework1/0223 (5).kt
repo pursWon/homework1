13f1e9d7ca22fb791db435a4a6d0c88553f9bae5
홍원기pc <dt95@naver.com>
2022-02-23 13:29:38 +0900

    2022.02.23 문자열 표현 - 큰 따옴표와 삼중 따옴표의 차이

---
+++ app/src/main/java/org/techtown/homework1/0223 (5).kt
@@ -0,0 +1,11 @@
+package org.techtown.homework1
+
+fun main () {
+  val str1 = "SON /n Kane"
+  val str2 = """
+        SON
+        KANE
+    """
+  println("str1 : $str1")
+  println("str2 : $str2")
+}
