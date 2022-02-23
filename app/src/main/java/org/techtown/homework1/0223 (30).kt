c872934c3de98334b4a91c4c0b8cd5c9eec47165
홍원기pc <dt95@naver.com>
2022-02-24 01:11:33 +0900

    2022.02.23 함수 타입 별칭

---
+++ app/src/main/java/org/techtown/homework1/0223 (30).kt
@@ -0,0 +1,12 @@
+package org.techtown.homework1
+
+typealias MyFunType = (Int, Int) -> Boolean
+
+fun main() {
+    val someFun: MyFunType = { no1: Int, no2: Int ->
+        no1 > no2
+    }
+    println(someFun(10, 20))
+    println(someFun(10, 20))
+
+}
