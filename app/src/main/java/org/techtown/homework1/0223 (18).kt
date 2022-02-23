340410c5d5482facbfbfeb2e2c5b55a83b140a32
홍원기pc <dt95@naver.com>
2022-02-23 16:12:59 +0900

    2022.02.23 반복 조건에 컬렉션 타입 활용

--- app/src/main/java/org/techtown/homework1/0223 (18).kt
+++ app/src/main/java/org/techtown/homework1/0223 (18).kt
@@ -1,12 +1,11 @@
 package org.techtown.homework1
 
 fun main() {
-    var sum: Int = 0
-    for (i in 1..10) {
-        sum += i
+    var data = arrayOf<Int>(10, 20, 30)
+    for (i in data.indices) {
+        print(data[i])
+        if (i !== data.size - 1) print(",")
 
     }
-
-    println(sum)
 
 }
