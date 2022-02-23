a4130fb9a6cce3c382b0e34e3709f64d7ffffcf1
홍원기pc <dt95@naver.com>
2022-02-23 13:06:59 +0900

    2022.02.23 기초 데이터 타입

---
+++ app/src/main/res/layout/0223 (4).kt
@@ -0,0 +1,15 @@
+package layout
+
+val a1: Byte = 0b00001011
+
+val a2: Int = 123
+
+val a3: Short = 123
+
+val a4: Long = 10L
+
+val a5: Double = 10.0
+
+val a6: Float = 10.0f
+
+val a7: Boolean = true
