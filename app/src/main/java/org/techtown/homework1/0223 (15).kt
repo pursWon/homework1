e82774b840ef44c3d6a3dfd169fbbd1a39ea80ef
홍원기pc <dt95@naver.com>
2022-02-23 15:44:15 +0900

    2022.02.23 when 문 사용 예시

---
+++ app/src/main/java/org/techtown/homework1/0223 (15).kt
@@ -0,0 +1,12 @@
+package org.techtown.homework1
+
+fun main () {
+    var data = 20
+    when (data) {
+        10 -> println("data is 10")
+        20 -> println("data is 20")
+        else -> {
+        println("data is not valid")
+        }
+    }
+}
