f907c67b9abe60f94ca3b5c3c24b6c9378825861
홍원기pc <dt95@naver.com>
2022-02-23 14:43:09 +0900

    2022.02.23 null 허용과 불허용

---
+++ app/src/main/java/org/techtown/homework1/0223 (8).kt
@@ -0,0 +1,13 @@
+package org.techtown.homework1
+
+
+fun main() {
+
+
+    var data1: Int = 10
+    data1 = null // ?가 없어서 null이 성립안됨
+
+    var data2: Int? = 10
+    data2 = null // ?가 있어야 null이 성립
+
+}
