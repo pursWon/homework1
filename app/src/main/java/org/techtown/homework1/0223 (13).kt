d1e03d08ca879f3d7777b683767f6ac523330f2c
홍원기pc <dt95@naver.com>
2022-02-23 15:33:16 +0900

    2022.02.23 if ~ else 사용 예시문

---
+++ app/src/main/java/org/techtown/homework1/0223 (13).kt
@@ -0,0 +1,15 @@
+package org.techtown.homework1
+
+fun main() {
+    var data = 10
+    if (data > 10) {
+        println("data > 10")
+    } else if (data > 0 && data <=10) {
+        println("data > 0 && data <= 10")
+    } else {
+        println("data <= 0")
+    }
+}
+
+
+// 실행 결과 data > 0 && data <=10
